8875ffbf2c3336bba4b9f04db376542c94f0e049
kodebae <heykarmendurbin@gmail.com>
2023-05-08 17:10:16 -0400

    feat: navbar features

--- manga-app/src/App.tsx
+++ manga-app/src/App.tsx
@@ -3,9 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Chocobo Manga Cafe</h1>
-    </div>
+    <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
+      <div className='container-fluid'>
+        <span className='navbar-brand'>The Manga Shelf</span>
+        <button className='navbar-toggler'type='button'
+        data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown'
+        aria-controls='navbarNavDropdown' aria-extended='false'
+        aria-label='Toggle Navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+          <div className='collapse navbar-collapse' id='navbarNavDropdown'>
+
+          </div>
+        </button>
+      </div>
+
+    </nav>
   );
 }
 

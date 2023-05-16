f4283b353d5b6a96b7adca3a1321c78c96f7337f
kodebae <heykarmendurbin@gmail.com>
2023-05-16 16:58:07 -0400

    styles: color updates

--- manga-app/src/Layouts/Home/Header.tsx
+++ manga-app/src/Layouts/Home/Header.tsx
@@ -1,9 +1,10 @@
 export const Header = () => {
     return (
-        <div className='p-5 mb-4 bg-dark header'>
-            <div className='container-fluid py-5 text-white d-flex justify-content-center align-items-center'>
+        <div className='jumbotron p-5 mb-4 bg-dark header'>
+            <div className='container-fluid py-7 text-white d-flex justify-content-center align-items-center'>
                 <div>
-                    <h1 className='display-5 fw-bold'>Main Header Text</h1>
+                    <h1 className='display-4 fw-bold'>Main Header Text</h1>
+                    <hr className="my-4"></hr>
                     <h5 className='col-md-8 fs-4'>Smaller Bold Text</h5>
                     <a type='button' className='btn main-color btn-lg text-white' href='#'>Featured Manga</a>
                 </div>
